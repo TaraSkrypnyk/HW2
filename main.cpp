e50bd0c864d75bef37e198696e1289f1e352edad
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 22:36:45 +0200

    Ex3 was done

--- main.cpp
+++ main.cpp
@@ -3,3 +3,18 @@
 #include <ctime>
 #include <cmath>
 using namespace std;
+
+int main() {
+    double max = 0, a;
+    for (int i = 0; i < 7; i++)
+    {
+        cout << "Vvedit " << i+1 << " chislo: ";
+        cin >> a;
+        if (a > max)
+        {
+            max = a;
+        }
+    }
+    cout <<"Max = " << max;
+    return 0;
+}
