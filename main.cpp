29b572593bb41a1466efdfe1ef8bad22ef51b081
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 19:24:01 +0200

    Ex1 was done

--- main.cpp
+++ main.cpp
@@ -3,3 +3,26 @@
 #include <ctime>
 #include <cmath>
 using namespace std;
+
+int main() {
+    int a, l=0, r=0;
+    cout << "Vvedit cile shestyznachne chislo: ";
+    cin >> a;
+    while (a > 999999 || a < 100000)
+    {
+        cout << "Vvedit cile shestyznachne chislo: ";
+        cin >> a;
+    }
+    l = (a / 100000) + ((a / 10000) % 10) + ((a / 1000) % 10);
+    r = a%10 + ((a / 100) % 10) + ((a / 10) % 10);
+    if (l == r)
+    {
+        cout << "Vashe chislo shchaslyve" << endl;
+
+    }
+    else
+    {
+        cout << "Vashe chislo ne shchaslyve" << endl;
+    }
+    return 0;
+}
