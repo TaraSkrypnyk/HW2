a68560f7ab6e5d8a68c90ecc99d67fe221b3bc8d
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 19:29:09 +0200

    Ex2 was done

--- main.cpp
+++ main.cpp
@@ -3,3 +3,20 @@
 #include <ctime>
 #include <cmath>
 using namespace std;
+
+int main() {
+    int a, x1, x2, x3, x4;
+    cout << "Vvedit cile chotyryznachne chislo: ";
+    cin >> a;
+    while (a > 9999 || a < 1000)
+    {
+        cout << "Vvedit cile chotyryznachne chislo: ";
+        cin >> a;
+    }
+    x1 = a / 1000;
+    x2 = (a / 100) % 10;
+    x3 = (a / 10) % 10;
+    x4 = a % 10;
+    cout << x2 << x1 << x4 << x3;
+    return 0;
+}
